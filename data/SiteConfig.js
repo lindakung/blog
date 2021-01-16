81e179ff66a90a63b512ae95810668e7c83cd8d2
Linda Kung <lindakung417@gmail.com>
2021-01-16 17:37:06 -0500

    Remove footer and header

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,15 +1,11 @@
 const config = {
-  siteTitle: 'Gatsby Markdown Blog Starter', // Site title.
-  siteTitleShort: 'Gatsby Markdown Blog Starter', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: 'Gatsby Markdown Blog Starter', // Alternative site title for SEO.
+  siteTitle: 'blog', // Site title.
+  siteTitleShort: 'blog', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: 'blog', // Alternative site title for SEO.
   siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
   siteUrl: 'https://gatsby-markdown-blog-starter.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '/blog', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription:
-    'A Gatsby Markdown Blog stater with Advanced design in mind.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml', // Path to the RSS file.
-  siteFBAppID: '1825356251115265', // FB Application ID for using app insights
-  googleAnalyticsID: 'UA-161211056-1', // GA tracking ID.
   dateFromFormat: 'YYYY-MM-DD', // Date format used in the frontmatter.
   dateFormat: 'DD/MM/YYYY', // Date format for display.
   userName: 'Jone Due', // Username to display in the author segment.
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,28 +5,7 @@ import Categories from './Categories'
 import styles from './Header.module.scss'
 
 const Header = () => (
-  <header>
-    <h1>
-      <Link to="/" activeClassName={styles.activeNav}>
-        {config.siteTitle}
-      </Link>
-    </h1>
-    <nav>
-      <ul className={styles.mainNav}>
-        <li>
-          <Link to="/about" activeClassName={styles.activeNav}>
-            About
-          </Link>
-        </li>
-        <Categories activeClassName={styles.activeNav} />
-        <li>
-          <Link to="/contact" activeClassName={styles.activeNav}>
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
+  <header />
 )
 
 export default Header
